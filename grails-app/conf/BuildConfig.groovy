a834ec274cc4ec5ae78c6801f219b2cc8afb9460
Thomas Wang <memmener@yahoo.com>
2013-03-25 10:52:54 -0700

    add org.scribe package to compile dependencies in BuildConfig.groovy. create YelpApi2 service provider in src/java

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,6 +42,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 
         // runtime 'mysql:mysql-connector-java:5.1.22'
+		compile 'org.scribe:scribe:1.3.2'
     }
 
     plugins {
