1626ee17c128927ae4e00768a7dba25dd73d5bc6
Thomas Wang <memmener@yahoo.com>
2013-03-26 11:37:59 -0700

    yelp controller shortsearch returns address and categories

--- grails-app/controllers/com/cardsmart/YelpController.groovy
+++ grails-app/controllers/com/cardsmart/YelpController.groovy
@@ -26,13 +26,26 @@ class YelpController {
 		def jsonText = yelpSearchService.search(params)
 		def json = new JsonSlurper().parseText(jsonText)
 		def results = [:]
+		def businesslist = []
 		json.businesses.each() { it ->
-			def props = [:]
-			props.name = it.name
-			props.lat = it.location.coordinate.latitude
-			props.lng = it.location.coordinate.longitude
-			results[(it.id)] = props
+			def business = [:]
+			def categorieslist = []
+			String address = ""
+			business.name = it.name
+			business.lat = it.location.coordinate.latitude
+			business.lng = it.location.coordinate.longitude
+			it.location.address.each() { item ->
+				address = address + item + " "
+			}
+			if (address.length() > 0) address = address.substring(0, address.length()-1)
+			business.address = address + ", " + it.location.city + ", " + it.location.state_code
+			it.categories.each() { item ->
+				categorieslist << item[1]
+			}
+			business.categories = categorieslist
+			businesslist << business
 		}
+		results.businesses = businesslist
 		render results as JSON
 	}
 }
