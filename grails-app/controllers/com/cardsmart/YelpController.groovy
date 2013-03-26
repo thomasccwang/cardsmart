725905fae09bb93b472eeab0d30c9509303cb409
Thomas Wang <memmener@yahoo.com>
2013-03-26 11:41:52 -0700

    remove unused actions from yelpcontroller

--- grails-app/controllers/com/cardsmart/YelpController.groovy
+++ grails-app/controllers/com/cardsmart/YelpController.groovy
@@ -4,22 +4,11 @@ import groovy.json.*
 import grails.converters.*
 
 class YelpController {
-	
+
 	def yelpSearchService
-	
-    def index() {
+    
+	def index() {
 		render "default page"
-	}
-	
-	def search() {
-		def result = yelpSearchService.search(params)
-		render result
-	}
-	
-	def formattedsearch() {
-		def jsonText = yelpSearchService.search(params)
-		def json = new JsonSlurper().parseText(jsonText)
-		render json as JSON
 	}
 	
 	def shortsearch() {
