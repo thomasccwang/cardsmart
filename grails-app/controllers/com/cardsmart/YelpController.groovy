3c9a2fb2bbefd1849506a558d82a81814b0912e7
Thomas Wang <memmener@yahoo.com>
2013-03-25 17:43:14 -0700

    add search that returns yelp results subset as JSON

--- grails-app/controllers/com/cardsmart/YelpController.groovy
+++ grails-app/controllers/com/cardsmart/YelpController.groovy
@@ -21,4 +21,18 @@ class YelpController {
 		def json = new JsonSlurper().parseText(jsonText)
 		render json as JSON
 	}
+	
+	def shortsearch() {
+		def jsonText = yelpSearchService.search(params)
+		def json = new JsonSlurper().parseText(jsonText)
+		def results = [:]
+		json.businesses.each() { it ->
+			def props = [:]
+			props.name = it.name
+			props.lat = it.location.coordinate.latitude
+			props.lng = it.location.coordinate.longitude
+			results[(it.id)] = props
+		}
+		render results as JSON
+	}
 }
